f109d9d96b452c35451c2990e249c62af93c0d28
matheushr-dev <78974230+matheushr-dev@users.noreply.github.com>
2021-02-27 20:59:52 -0300

    Inclusão do projeto

---
+++ Models/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using Pokedex.Utils;
+
+namespace Pokedex.Models
+{
+    [DataContract]
+    public class Pokemon
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember(Name = "name")]
+        private string nome;
+        public string Nome //Deixa a primeira leitra em maiuscula
+        {    
+            get { return Util.FirstCharToUpper(this.nome); }
+            set { Nome = this.nome; }
+        }
+
+        [DataMember(Name = "height")]
+        private double altura;
+        public double Altura { get { return this.altura / 10; } set { this.Altura = altura; } }
+
+        [DataMember(Name = "weight")]
+        private double peso;
+        public double Peso { get { return this.peso / 10; } set { this.Peso = peso; } }
+
+        [DataMember(Name = "types")]
+        public PokemonTipo[] Tipos { get; set; }
+    }
+}
